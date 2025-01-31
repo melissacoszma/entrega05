7a77e8f39b23b21032a2ddddfd36cb468319b157
Melisa <melisacozma1234@gmail.com>
2025-01-31 08:51:18 +0100

    js primera revision

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import "./style.css";
 
 console.log("Hello from main");
 
-let puntos;
+let puntos = 0;
 
-puntos = 0;
+const botonPedir = document.getElementById("pidoCarta");
+const botonMePlanto = document.getElementById("mePlanto");
+const botonNuevoJuego = document.getElementById("nuevo");
+const botonUnaMas = document.getElementById("unaMas");
+
 
 const muestraPuntuacion = () => {
     const puntuacion = document.getElementById("puntuacion");
@@ -25,7 +29,6 @@ const pidoCarta = () =>{
     let carta = randomCarta();
     let valorCarta;
 
-    // Asignar el valor de la carta
     if (carta >= 7) {
         valorCarta = 0.5; 
     } else {
@@ -37,20 +40,60 @@ const pidoCarta = () =>{
     }else {
         puntos += 0.5;
     };
-
-
-    gameOver();
+    
     muestraPuntuacion();
     mostrarCarta(carta);
     console.log("Carta generada:", carta, "Valor de la carta:", valorCarta);   
+    winOrLost();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    const botonPedir = document.getElementById("pidoCarta");
+    botonNuevoJuego.hidden = true;
+    botonUnaMas.hidden = true;
+    botonMePlanto.disabled = false;
+    
+    const desactivaPideCarta = (boton) =>{
+        boton.disabled = true;
+    };
+
     if (botonPedir) {
         botonPedir.addEventListener("click", pidoCarta);
-    }
+    };
+
+    if (botonMePlanto) {
+        botonMePlanto.addEventListener("click", () => {
+            desactivaPideCarta(botonPedir); 
+            gameStatusOnPlantar(); 
+            botonNuevoJuego.hidden = false; 
+            botonUnaMas.hidden = false;
+        });
+    };
+
+    if (botonNuevoJuego) {
+        botonNuevoJuego.addEventListener("click", () => {
+            puntos = 0;
+            mensaje.textContent = "";
+            mostrarCarta(0);
+            muestraPuntuacion();
+            botonNuevoJuego.hidden = true;
+            botonPedir.disabled = false; 
+            botonUnaMas.hidden = true; 
+            botonMePlanto.disabled = false;
+        });
+    };
+
+    if (botonUnaMas) {
+        botonUnaMas.addEventListener("click", () => {
+            muestraPuntuacion();
+            pidoCarta();
+            botonUnaMas.disabled = true;
+        });
+    };
+    
 });
+
+      
+
 
 const mostrarCarta = (carta) => {
     const imgCarta = document.getElementById("imgCarta");
@@ -58,6 +101,9 @@ const mostrarCarta = (carta) => {
 
     
     switch (carta) {
+        case 0:
+            srcCarta = "./img/back.jpg";
+            break;
         case 1:
             srcCarta = "./img/1_as-copas.jpg";
             break;
@@ -92,29 +138,57 @@ const mostrarCarta = (carta) => {
             srcCarta = "./img/back.jpg"; // Carta boca abajo por defecto, el html inicia en esta también
     }
 
-    // Actualizar el src de la imagen
+    
     imgCarta.src = srcCarta;
 };
 
-    
+const winOrLost = () => {                         //lo ejecutaremos cada vez q se pida carta para comprobar si s eha ganado o perdido
+    if (puntos > 7.5) {
+        mensaje.textContent = "Has perdido :(";
+        console.log("Has perdido");
+        botonPedir.disabled = true;
+        botonMePlanto.disabled = true;
+        botonNuevoJuego.hidden = false;
+        botonUnaMas.hidden = true;
+    }
 
-
-const gameOver = () => {
-    if(puntos > 7.5){
-        const mensaje = document.getElementById("mensaje");
-        mensaje.textContent = "has perdido";
-        console.log("has perdido");
+    if (puntos === 7.5) {
+        mensaje.textContent = "¡Enhorabuena, has ganado :D!";
+        console.log("Has ganado");
+        botonPedir.disabled = true;
+        botonMePlanto.disabled = true;
+        botonNuevoJuego.hidden = false;
+        botonUnaMas.hidden = true;
     }
-    if(puntos <= 4){
-        mensaje.textContent =  "has sido muy conservador";
-    }
-    if(puntos >=5 && puntos < 6 ){
-        mensaje.textContent = "te ha entrado el canguelo";
-    }
-    if(puntos >= 6 && puntos <= 7){
-        mensaje.textContent = "casi, casi";
-    }
-    if(puntos == 7.5){
-        mensaje.textContent = "enhorabuena, has ganado"
-    };
 }
+
+const gameStatusOnPlantar = () => {
+    const mensaje = document.getElementById("mensaje");
+
+    switch (true) {                                        //Para pasar la constante a un switch he tenido q usar una booleana, evalua si cada caso es verdadero y ejecuta solamente el q lo es
+        /*case puntos > 7.5:
+            mensaje.textContent = "Has perdido :(";         // quito los estados que quiero que se ejecuten automaticamente para pasarlos a una funcion exclusiva
+            console.log("Has perdido");
+            break;*/
+
+        case puntos <= 4:
+            mensaje.textContent = "Has sido muy conservador u.u";
+            break;
+
+        case puntos >= 5 && puntos < 6:
+            mensaje.textContent = "Te ha entrado el canguelo :S";
+            break;
+
+        case puntos >= 6 && puntos <= 7:
+            mensaje.textContent = "Casi, casi o.o";
+            break;
+
+        /*case puntos === 7.5:
+            mensaje.textContent = "¡Enhorabuena, has ganado :D!";
+            break;*/
+
+        default:
+            mensaje.textContent = "";
+            break;
+    }
+};
